61f91c1718c746e1fa9ac7289d40541e3438bd36
Cyan <dawid.grygier5@gmail.com>
2018-12-10 10:37:35 +0100

    Maven project initialized

---
+++ src/main/java/java_advance/spring_boot_spotify/SpringBootSpotifyApplication.java
@@ -0,0 +1,12 @@
+package java_advance.spring_boot_spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpotifyApplication.class, args);
+    }
+}
---
+++ src/test/java/java_advance/spring_boot_spotify/SpringBootSpotifyApplicationTests.java
@@ -0,0 +1,16 @@
+package java_advance.spring_boot_spotify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSpotifyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

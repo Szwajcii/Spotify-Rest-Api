8cf988a9dea7d1ae3d36fc51160524df1e45c374
Czakero <bartosz.pyrz96@gmail.com>
2018-12-10 15:05:39 +0100

    Mark repository with Repository annotation

--- src/main/java/java_advance/spring_boot_spotify/repository/UserRepository.java
+++ src/main/java/java_advance/spring_boot_spotify/repository/UserRepository.java
@@ -2,6 +2,8 @@ package java_advance.spring_boot_spotify.repository;
 
 import java_advance.spring_boot_spotify.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }

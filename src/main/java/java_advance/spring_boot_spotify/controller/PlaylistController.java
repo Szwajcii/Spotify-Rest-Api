f29b1654664732ab33102522631b667faa722b87
Szwajcii <d.szwajkos@gmail.com>
2018-12-10 14:23:38 +0100

    Implement playlist controller class.

---
+++ src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
@@ -0,0 +1,10 @@
+package java_advance.spring_boot_spotify.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlaylistController {
+
+
+
+}

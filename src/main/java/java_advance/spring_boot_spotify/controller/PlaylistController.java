ed1bfb8f77cfa795b20895a135d7f4983ff8083c
Szwajcii <d.szwajkos@gmail.com>
2018-12-10 15:03:01 +0100

    Create method to retrive all songs by given type from playlist.

--- src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
+++ src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
@@ -1,6 +1,9 @@
 package java_advance.spring_boot_spotify.controller;
 
+import java_advance.spring_boot_spotify.model.Song;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class PlaylistController {
@@ -9,6 +12,10 @@ public class PlaylistController {
 
     public Collection<Song> getAllSongs(){
         return playlistService.getAllSongs();
+    }
+
+    public Collection<Song> getSongsByGenre(String genre){
+        return playlistService.getSongsByGenre(genre);
     }
 
 }

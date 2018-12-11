21721bb54726f0ada95cd92dfc23da36dbdf1b11
Szwajcii <d.szwajkos@gmail.com>
2018-12-11 11:34:54 +0100

    Implement method to get all songs of given playlist.

--- src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
+++ src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
@@ -1,8 +1,11 @@
 package java_advance.spring_boot_spotify.controller;
 
 import java_advance.spring_boot_spotify.model.Playlist;
+import java_advance.spring_boot_spotify.model.Song;
 import java_advance.spring_boot_spotify.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,10 +20,15 @@ public class PlaylistController {
         this.playlistService = playlistService;
     }
 
+    @GetMapping("/all")
     public List<Playlist> getAllPlaylists(){
         return playlistService.getAllPlaylists();
     }
 
+    @GetMapping("/all/{playlistId}")
+    public Iterable<Song> getAllSongs(@PathVariable Long playlistId){
+        return playlistService.getAllSongs(playlistId);
+    }
 
 
 }

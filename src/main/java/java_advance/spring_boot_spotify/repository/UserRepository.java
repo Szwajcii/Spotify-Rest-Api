0b5c64eb108459795cfa1fc91e5f6151e007b276
Czakero <bartosz.pyrz96@gmail.com>
2018-12-11 13:29:13 +0100

    Find all returns list tha iterable now

--- src/main/java/java_advance/spring_boot_spotify/repository/UserRepository.java
+++ src/main/java/java_advance/spring_boot_spotify/repository/UserRepository.java
@@ -4,6 +4,10 @@ import java_advance.spring_boot_spotify.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    @Override
+    List<User> findAll();
 }

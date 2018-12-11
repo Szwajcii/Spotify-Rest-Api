122aa12fe06c5255c6c40edf60a077ae96fc9e7e
Szwajcii <d.szwajkos@gmail.com>
2018-12-11 11:11:29 +0100

    Implement constructor and method to retrive all playlist in playlist controller.

--- src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
+++ src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
@@ -1,10 +1,25 @@
 package java_advance.spring_boot_spotify.controller;
 
+import java_advance.spring_boot_spotify.model.Playlist;
+import java_advance.spring_boot_spotify.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PlaylistController {
 
+    private PlaylistService playlistService;
+
+    @Autowired
+    public PlaylistController(PlaylistService playlistService){
+        this.playlistService = playlistService;
+    }
+
+    public List<Playlist> getAllPlaylists(){
+        return playlistService.getAllPlaylists();
+    }
 
 
 

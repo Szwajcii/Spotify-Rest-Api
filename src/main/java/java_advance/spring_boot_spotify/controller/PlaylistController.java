2cdfec26121f0626190f6e87ce55444c2c7af17b
Szwajcii <d.szwajkos@gmail.com>
2018-12-11 10:24:34 +0100

    Implement new interface.

--- src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
+++ src/main/java/java_advance/spring_boot_spotify/controller/PlaylistController.java
@@ -2,6 +2,7 @@ package java_advance.spring_boot_spotify.controller;
 
 import java_advance.spring_boot_spotify.model.Playlist;
 import java_advance.spring_boot_spotify.model.Song;
+import java_advance.spring_boot_spotify.service.PlaylistService;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
---
+++ src/main/java/java_advance/spring_boot_spotify/service/PlaylistServiceInterface.java
@@ -0,0 +1,7 @@
+package java_advance.spring_boot_spotify.service;
+
+public interface PlaylistServiceInterface {
+
+
+
+}
